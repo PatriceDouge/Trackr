fbf597537c8cad7fbf0b15b646b08ac4ec0d20d7
PatriceDouge <patricedouge@gmail.com>
2019-11-06 21:50:03 -0500

    base crud operations done

--- trackr-app/backend/settings.py
+++ trackr-app/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('trackr_db'),
+        'USER': os.environ.get('patrice'),
+        'PASSWORD': os.environ.get('123456'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
