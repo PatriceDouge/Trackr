c19241371f59112c93d31903317bb038a2b48411
PatriceDouge <patricedouge@gmail.com>
2019-11-11 19:11:02 -0500

    connecting redux with react

--- trackr-app/frontend/src/components/App.js
+++ trackr-app/frontend/src/components/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import Navbar from "./layout/Navbar";
 import Dashboard from "./dashboard/Dashboard";
+import store from "../store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="app">
-          <Navbar />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="app">
+            <Navbar />
+            <Dashboard />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- trackr-app/frontend/src/components/dashboard/Projects.js
+++ trackr-app/frontend/src/components/dashboard/Projects.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { getProjects } from "../../actions/projects";
 
 export class Projects extends Component {
+  static propTypes = {
+    projects: PropTypes.array.isRequired
+  };
+
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
     return (
       <div>
-        <h1>Project List</h1>
+        <h1> Project List </h1>
       </div>
-    )
+    );
   }
 }
 
-export default Projects
+const mapStateToProps = state => ({
+  projects: state.projects.projects
+});
+
+export default connect(
+  mapStateToProps,
+  { getProjects }
+)(Projects);
