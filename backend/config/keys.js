a2385e4a6e5f40c07795991739acc345794f55b2
PatriceDouge <patricedouge@gmail.com>
2020-02-11 22:44:52 -0500

    register/login backend done

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://patrice:poupon01@cluster0-fuqvh.mongodb.net/test?retryWrites=true&w=majority"
+  mongoURI: "mongodb+srv://patrice:poupon01@cluster0-fuqvh.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -24,6 +26,15 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 3000;
 
